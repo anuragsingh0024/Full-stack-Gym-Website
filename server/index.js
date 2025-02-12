1b8e4f9f6533836c3ac00be3c117586c2d5b15e6
anuragsingh0024 <anuragsinghrajput450@gmail.com>
2025-02-13 03:00:04 +0530

    cors solving

--- server/index.js
+++ server/index.js
@@ -27,6 +27,15 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "https://full-stack-gym-website-rho.vercel.app");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
