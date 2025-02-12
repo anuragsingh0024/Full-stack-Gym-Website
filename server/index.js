4b7b26c926145325764a827ade0aa34bfdf5fae7
anuragsingh0024 <anuragsinghrajput450@gmail.com>
2025-02-13 02:55:46 +0530

    all origin permision

--- server/index.js
+++ server/index.js
@@ -20,7 +20,7 @@ cloudinaryConnect();
 // Enable CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://full-stack-gym-website-rho.vercel.app/"],
+    origin: ["*"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"], // Allow specific HTTP methods
     allowedHeaders: ["Content-Type", "Authorization"], // Allow necessary headers
