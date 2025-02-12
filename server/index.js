c3d2d7eac2bbf786e7cd39bd82d9e55c1b3fc3e6
anuragsingh0024 <anuragsinghrajput450@gmail.com>
2025-02-13 02:34:00 +0530

    set origin in backend

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(
 // Enable CORS
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://full-stack-gym-website-rho.vercel.app/"],
     credentials: true,
   })
 );
